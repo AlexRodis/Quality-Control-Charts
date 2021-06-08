73c9d698be8aeef1298119398d3c030fd229e410
Alexander Rodis <alexanderrhodis@gmail.com>
2021-06-08 12:08:09 +0300

    Added docstrings to functions

--- scripts.py
+++ scripts.py
@@ -20,6 +20,17 @@ class MovingMean:
     Vector = list[float]
     
     def __init__(self, start_pts : Vector, new_pts : Vector)->None:
+        '''
+        Base class constructor.
+        Args:
+            start_pts: list[float] A list of N initial measurements without indices
+            new_pts: list[float] A list of subsequent measurements without indices
+
+        returns:
+            None
+
+        '''
+
         self.active_pts = deque(start_pts)
         self._cnt = len(self.active_pts)
         self.new_pts = deque(new_pts)
@@ -40,14 +51,41 @@ class MovingMean:
         
         
     def __init_table(self)->None:
+        '''
+        Reshapes self._line_data which contains y coords for all horizontal decision lines by 
+        adding column names
+        Args:
+            self
+        Returns:
+            None
+        '''
         self._line_data.columns = ["upper_action","lower_action","upper_warning","lower_warning","average","idx"]
 
 
     def __reshape_table(self)->None:
+        '''
+        Reshapes table by declaring column idx as index
+
+        Args:
+            self
+
+        Return
+            None
+        '''
         self._line_data = self._line_data.astype({"idx":"int32"})
         self._line_data = self._line_data.set_index('idx')
 
     def __set_table(self,graph_vars : namedtuple)->None:
+        '''
+        Set all decision line points for decision lines
+
+        Args
+            self
+            graph_vars: namedtuple[Vars, [mu,sd]] A named tuple with pairs of [mu,sd] for each record
+
+        Returns:
+            None
+        '''
         new_vars = {
             "upper_action":graph_vars.mu+3*graph_vars.sd,
             "lower_action":graph_vars.mu-3*graph_vars.sd,
@@ -59,10 +97,27 @@ class MovingMean:
         self._line_data = self._line_data.append(new_vars,ignore_index=True)
         
     def __bitshift(self)->None:
+        '''
+        Pop the oldest point for active, pop the youngest point in self.new_pts and insert it
+        into self.active. Deprecated
+        Args
+            self
+        Returns
+            None
+        '''
         self.active_pts.append(self.new_pts.pop())
         self.past_pts.append(self.active_pts.popleft())
     
     def __get_params(self)->namedtuple:
+        '''
+        Compute sd and mu parameters for each new observation and add them to a list
+
+        Args
+            self
+        
+        Returns
+            namedtuple[Vars, [mu,sd]] A namedtuple of mu,sd pairs
+        '''
         vars = namedtuple("Vars", ['mu','sd'])
         c = self.active_pts
         m = mean(c)
@@ -76,6 +131,20 @@ class MovingMean:
 
 
     def __render__(self)->None:
+        '''
+        Method called to render final figure
+
+        Args
+            self
+        
+        Returns
+            None
+        
+        Exports
+            Matplotlib.figure The QC Plot
+        '''
+        # This needs to be broken up to a sepperate function/class/object to add basic parameters to the diagram,
+        # keeping only the basic border lines here
         fig,ax = plt.subplots()
         ax.set_xlim(self.lower_bound,self._upper_bound)
         for idx,col in self._line_data.iterrows():
@@ -105,6 +174,15 @@ class MovingMean:
             
             
     def __call__(self)->None:
+        '''
+        Once basic parameters are set, call the object to initiate computations
+
+        Args
+            self
+        Returns
+            None
+        '''
+        # This may need to be broken up to compute and render steps
         graph_vars = self.__get_params()
         self.__set_table(graph_vars)
         new_pts = deepcopy(self.new_pts)
